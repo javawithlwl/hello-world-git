2796ff84815e19dd06d1fb4a84cd078ecef585f1
javawithlwl <javawithlwl@gmail.com>
2023-03-08 07:14:59 +0530

    Added common dependency and changed validation of account number

--- src/main/java/com/careerit/helloworldgit/util/CommonUtil.java
+++ src/main/java/com/careerit/helloworldgit/util/CommonUtil.java
@@ -1,12 +1,14 @@
 package com.careerit.helloworldgit.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 public final class CommonUtil {
 
         private CommonUtil(){
 
         }
         public static String maskAccountNumber(String accNumber){
-            if(accNumber!=null && accNumber.equals("") && accNumber.length()>=4) {
+            if(StringUtils.isNoneEmpty(accNumber) && accNumber.length()>=4) {
                return "********" + accNumber.substring(accNumber.length() - 4);
             }
             return accNumber;
