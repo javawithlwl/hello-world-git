fe00de56e3b2ff3551f97e0bef5d3df13b84cc9d
javawithlwl <javawithlwl@gmail.com>
2023-03-08 06:20:38 +0530

    learning git basics - first commit

---
+++ src/main/java/com/careerit/helloworldgit/HelloWorldGitApplication.java
@@ -0,0 +1,13 @@
+package com.careerit.helloworldgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerit/helloworldgit/HelloWorldGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerit.helloworldgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
